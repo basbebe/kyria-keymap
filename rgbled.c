b4179e43c17bf2fa49e8d3069b15f2437c310297
basbebe <git@bastianbeuttel.com>
2021-09-27 12:01:10 +0200

    add NUM LED

--- rgbled.c
+++ rgbled.c
@@ -2,24 +2,25 @@
 #include "keymap.h"
 
 
-// Light LEDs 0 to 10 when  SYM_L is active
-const rgblight_segment_t PROGMEM _RGB_CAPS_LOCK[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}  // Light 4 LEDs, starting with LED 6
+const rgblight_segment_t PROGMEM _RGB_CAPS_LOCK[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}
+);
+
+const rgblight_segment_t PROGMEM _RGB_OK[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}
+);
+
+const rgblight_segment_t PROGMEM _RGB_QWERTY[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}
 );
 
 // Light LEDs 0 to 10 when  SYM_L is active
-const rgblight_segment_t PROGMEM _RGB_OK[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}  // Light 4 LEDs, starting with LED 6
+const rgblight_segment_t PROGMEM _RGB_SYM_L[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}
 );
 
-// Light LEDs 0 to 10 when  SYM_L is active
-const rgblight_segment_t PROGMEM _RGB_QWERTY[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}  // Light 4 LEDs, starting with LED 6
+// Light LEDs 11 to 20 when  SYM_R is active
+const rgblight_segment_t PROGMEM _RGB_SYM_R[] = RGBLIGHT_LAYER_SEGMENTS({10, 10, HSV_PURPLE}
 );
 
-// Light LEDs 0 to 10 when  SYM_L is active
-const rgblight_segment_t PROGMEM _RGB_SYM_L[] = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_PURPLE}  // Light 4 LEDs, starting with LED 6
-);
-
-// Light LEDs 0 to 10 when  SYM_L is active
-const rgblight_segment_t PROGMEM _RGB_SYM_R[] = RGBLIGHT_LAYER_SEGMENTS({10, 10, HSV_PURPLE}  // Light 4 LEDs, starting with LED 6
+// Light LEDs 11 to 20 when NUM is active
+const rgblight_segment_t PROGMEM _RGB_NUM[] = RGBLIGHT_LAYER_SEGMENTS({10, 10, HSV_YELLOW}
 );
 
 // Now define the array of layers. Later layers take precedence
@@ -28,7 +29,8 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     _RGB_OK,
     _RGB_QWERTY,
     _RGB_SYM_L,
-    _RGB_SYM_R
+    _RGB_SYM_R,
+    _RGB_NUM
 );
 
 void keyboard_post_init_user(void) {
@@ -49,5 +51,6 @@ void keyboard_post_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(3, layer_state_cmp(state, _SYM_L));
     rgblight_set_layer_state(4, layer_state_cmp(state, _SYM_R));
+    rgblight_set_layer_state(5, layer_state_cmp(state, _NUM));
     return state;
 }
